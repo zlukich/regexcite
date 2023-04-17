5f8a904eca13f20d872b3b77297a1e800aa44521
zlukich <soboliev@campus.tu-berlin.de>
2023-04-17 10:55:23 +0200

    test

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -16,6 +16,11 @@
 #' y <- "192.168.0.1"
 #' str_split_one(y, pattern = stringr::fixed("."))
 #' strsplit1(x, split = ",")
-strsplit1 <- function(x, split) {
-  strsplit(x, split = split)[[1]]
+str_split_one <- function(string, pattern, n = Inf) {
+  stopifnot(is.character(string), length(string) <= 1)
+  if (length(string) == 1) {
+    stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
+  } else {
+    character()
+  }
 }
