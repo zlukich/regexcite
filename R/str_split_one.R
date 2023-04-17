0aa09c39bcde9fbb6642c2555b6816a7e6495fe9
zlukich <soboliev@campus.tu-berlin.de>
2023-04-17 10:55:12 +0200

    str_split_one

---
+++ R/str_split_one.R
@@ -0,0 +1,21 @@
+
+
+#' Split a string
+#'
+#' @param string A character vector with, at most, one element.
+#' @inheritParams stringr::str_split
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' str_split_one(x, pattern = ",")
+#' str_split_one(x, pattern = ",", n = 2)
+#'
+#' y <- "192.168.0.1"
+#' str_split_one(y, pattern = stringr::fixed("."))
+#' strsplit1(x, split = ",")
+strsplit1 <- function(x, split) {
+  strsplit(x, split = split)[[1]]
+}
---
+++ tests/testthat/test-str_split_one.R
@@ -0,0 +1,3 @@
+test_that("multiplication works", {
+  expect_equal(strsplit1("a,b,c", split = ","), c("a", "b", "c"))
+})
