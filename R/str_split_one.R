bbd2be2a93f9226ad343e9bf64825bc7478bdace
Sasha Soboliev <soboliev@campus.tu-berlin.de>
2023-05-08 16:26:42 +0200

    added new_function_name

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -16,6 +16,31 @@
 #' y <- "192.168.0.1"
 #' str_split_one(y, pattern = stringr::fixed("."))
 str_split_one <- function(string, pattern, n = Inf) {
+  .Deprecated("new_function_name")
+  stopifnot(is.character(string), length(string) <= 1)
+  if (length(string) == 1) {
+    stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
+  } else {
+    character()
+  }
+}
+#' Split a string
+#'
+#' @param string A character vector with, at most, one element.
+#' @inheritParams stringr::str_split
+#'
+#' @return A character vector.
+#' @export new_function_name str_split_one
+#' @aliases str_split_one
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' str_split_one(x, pattern = ",")
+#' str_split_one(x, pattern = ",", n = 2)
+#'
+#' y <- "192.168.0.1"
+#' str_split_one(y, pattern = stringr::fixed("."))
+#'
+new_function_name <- function(string, pattern, n = Inf) {
   stopifnot(is.character(string), length(string) <= 1)
   if (length(string) == 1) {
     stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
