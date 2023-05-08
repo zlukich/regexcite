f2e92bd0435eb822a06c9625d38728e19b190e34
Sasha Soboliev <soboliev@campus.tu-berlin.de>
2023-05-08 16:48:22 +0200

    rdnae

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -1,6 +1,21 @@
 
 
-
+#' @rdname package-deprecated
+#'
+#' @param string A character vector with, at most, one element.
+#' @inheritParams stringr::str_split
+#'
+#' @return A character vector.
+#' @export new_function_name str_split_one
+#' @aliases str_split_one
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' new_function_name(x, pattern = ",")
+#' new_function_name(x, pattern = ",", n = 2)
+#'
+#' y <- "192.168.0.1"
+#' new_function_name(y, pattern = stringr::fixed("."))
+#'
 str_split_one <- function(string, pattern, n = Inf) {
   .Deprecated("new_function_name")
   new_function_name()
