2fc3377c0cfa085202d79cf2a4b1e40fa8267ac8
Sasha Soboliev <soboliev@campus.tu-berlin.de>
2023-05-08 16:35:39 +0200

    deleted documentation for str_split_one()

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -1,28 +1,9 @@
 
 
-#' Split a string
-#'
-#' @param string A character vector with, at most, one element.
-#' @inheritParams stringr::str_split
-#'
-#' @return A character vector.
-#' @export
-#'
-#' @examples
-#' x <- "alfa,bravo,charlie,delta"
-#' str_split_one(x, pattern = ",")
-#' str_split_one(x, pattern = ",", n = 2)
-#'
-#' y <- "192.168.0.1"
-#' str_split_one(y, pattern = stringr::fixed("."))
+
 str_split_one <- function(string, pattern, n = Inf) {
   .Deprecated("new_function_name")
-  stopifnot(is.character(string), length(string) <= 1)
-  if (length(string) == 1) {
-    stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
-  } else {
-    character()
-  }
+  new_function_name()
 }
 #' Split a string
 #'
@@ -34,11 +15,11 @@ str_split_one <- function(string, pattern, n = Inf) {
 #' @aliases str_split_one
 #' @examples
 #' x <- "alfa,bravo,charlie,delta"
-#' str_split_one(x, pattern = ",")
-#' str_split_one(x, pattern = ",", n = 2)
+#' new_function_name(x, pattern = ",")
+#' new_function_name(x, pattern = ",", n = 2)
 #'
 #' y <- "192.168.0.1"
-#' str_split_one(y, pattern = stringr::fixed("."))
+#' new_function_name(y, pattern = stringr::fixed("."))
 #'
 new_function_name <- function(string, pattern, n = Inf) {
   stopifnot(is.character(string), length(string) <= 1)
